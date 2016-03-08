78c587681577dc7cbcf22e0bf72b603515b74eeb
alexanderanter <alexanderanter@gmail.com>
2016-03-08 14:20:19 +0100

    restructured min and max functions to instead extend the array

--- src/statistics.js
+++ src/statistics.js
@@ -15,40 +15,16 @@
  * @returns {{max: Number, mean: Number, median: Number, min: Number, mode: Array.<Number>, range: Number}}
  */
 
-exports.analyze = function(data) {
-    //error handling
-    if (typeof data !== "object") {
-        throw new TypeError("you didn't send in an array");
-    }else if (data.length < 1) {
-        throw new Error("the array is empty!");
-    }
+//extending the array prototype by adding .max and .min function and apply
+//max and min function invokes the math function and sends in the array.
 
-    var copiedArray  = data.slice();
-    var result = {};
-    var minVal = exports.getMin(copiedArray);
-    var maxVal = exports.getMax(copiedArray);
-    var meanVal = exports.getMean(copiedArray);
-    var medianVal = exports.getMedian(copiedArray);
-    var modeVal = exports.getMode(copiedArray);
-    var rangeVal = exports.getRange(copiedArray);
-
-    return result;
+Array.max = function(data) {
+    return Math.max.apply(Math, data);
 };
 
-exports.getMin = function(data) {
-    var result;
-    var copiedArray = data.slice();
-
-
-    return result;
-}
-
-exports.getMax = function(data) {
-    var result;
-    var copiedArray = data.slice();
-
-    return result;
-}
+Array.min = function(data) {
+    return Math.min.apply(Math, data);
+};
 
 exports.getMean = function(data) {
     var result;
@@ -77,3 +53,31 @@ exports.getRange = function(data) {
 
     return result;
 }
+
+exports.analyze = function(data) {
+    //error handling
+    if (typeof data !== "object") {
+        throw new TypeError("you didn't send in an array");
+    }else if (data.length < 1) {
+        throw new Error("the array is empty!");
+    }
+
+    var copiedArray  = data.slice();
+    var result = {};
+    var minVal = Array.min(copiedArray);
+    var maxVal = Array.max(copiedArray);
+    var meanVal = exports.getMean(copiedArray);
+    var medianVal = exports.getMedian(copiedArray);
+    var modeVal = exports.getMode(copiedArray);
+    var rangeVal = exports.getRange(copiedArray);
+
+    console.log("max value: " + maxVal);
+    console.log("min value: " + minVal);
+    console.log("mean value: " + maxVal);
+    console.log("median value: " + minVal);
+    console.log("mode value: " + minVal);
+    console.log("range value: " + minVal);
+
+
+    return result;
+};
